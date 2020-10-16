b582e270f801fb8d04e4b02b90bdff33fd2e572e
KimJiHu0 <65142418+KimJiHu0@users.noreply.github.com>
2020-10-16 22:44:55 +0900

    김지후 int=3을 없앴어!

--- src/main/java/com/mvc/homeseek/common/aop/homeseekLogAop.java
+++ src/main/java/com/mvc/homeseek/common/aop/homeseekLogAop.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class homeseekLogAop {
-	 int a = 3;
 	/*
 	 *  getTarget() : 대상 객체 (포인트랑 연결되어있는 cc는 누구인지 알려준다.) 
 	 *  getArgs() : 대상 파라미터들을 다 가지고 와준다.
