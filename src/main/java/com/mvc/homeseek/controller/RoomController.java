ca61c993defb2a43458266284f224b2d42199b84
ffolabear <kth6699@naver.com>
2020-10-20 09:23:54 +0900

    태현 : 매물올리기의 Model, Controller 만들고 메인에서매물올리기 페이지로 넘어가는 페이지 만듬

--- src/main/java/com/mvc/homeseek/controller/RoomController.java
+++ src/main/java/com/mvc/homeseek/controller/RoomController.java
@@ -2,9 +2,63 @@ package com.mvc.homeseek.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mvc.homeseek.model.dto.RoomDto;
+
+@Controller
 public class RoomController {
 	
 	private Logger logger = LoggerFactory.getLogger(RoomController.class);
+	
+	public String selectList(Model model) {
+		logger.info("[list.do]");
+		
+		return "";
+	}
+	
+	@RequestMapping("insertRoom.do")
+	public String insertRoom(RoomDto roomDto, Model model) {
+		logger.info("[insert.do] jsp로이동");
+		
+		return "RoomInsert";
+	}
+	
+	@RequestMapping("insertRes.do")
+	public String insertRes(RoomDto roomDto, Model model) {
+		logger.info("[inertres.do]");
+		
+		return "RoomInsertRes";
+	}
+
+//	public String detail(Model model, @RequestParam("room_no") int room_no) {
+//		
+//		logger.info("[detail.do]");
+//		
+//		return "";
+//	}
+	
+	public String delete(int room_no) {
+		
+		logger.info("[delete.do]");
+		
+		return "";
+	}
+	
+	public String updateForm(Model model, int room_no) {
+		logger.info("[update.do] jsp로 이동");
+		
+		return "";
+	}
+	
+	//@RequestMapping(value="/updateres.do", method=RequestMethod.POST)
+	public String updateRes(RoomDto roodDto) {
+		logger.info("[updateres.do]");
+		
+		return "";
+
+	}
 
 }
