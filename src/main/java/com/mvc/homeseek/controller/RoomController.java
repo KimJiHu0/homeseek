978dbb01567ba9d48c6ccac242e5e06ffcf080c0
KimJiHu0 <65142418+KimJiHu0@users.noreply.github.com>
2020-10-24 00:52:40 +0900

    김지후 : 태현이형 roomInsert.xml config에 추가했고, 태현이형꺼 RoomController, RoomDaoImpl 건들였고 RoomDetailController에 주석부분 다 지웠습니다.

--- src/main/java/com/mvc/homeseek/controller/RoomController.java
+++ src/main/java/com/mvc/homeseek/controller/RoomController.java
@@ -48,12 +48,24 @@ public class RoomController {
 	public String selectRoomInsert(RoomDto room_dto, Model model) {
 		logger.info("[inertres.do] : 매물 올리기 제출중");
 
+		// <input type="date"/> 의 value값의 타입은 String 타입이에요.
+		// 그리고 값은 2020-10-24 식으로 현재 이 컨트롤러로 들어오게 되고, RoomDto에서 cpdate와 avdate를 String 타입으로 선언이 되었기 때문에 파라미터로 RoomDto room_dto로
+		// 선언을 해도 잘 넘어와요.
 		
+		// 그럼 현재 room_dto에 담겨져있는 cpdate는 '2020-10-24'로 담겨져있고, 
+		// avdate는 '2020-10-24' 형태이고 둘 다 String 타입이에요.
+		
+		// 아래의 명령문들은 작성하지 않으셔도 돼요!! 어차피 원하는 값의 형태이기 때문이에요.
 		String prv_room_cpdate = room_dto.getRoom_cpdate().substring(0,10);
 		String prv_room_avdate = room_dto.getRoom_avdate().substring(0,10); 
 		logger.info(prv_room_cpdate); 
 		logger.info(prv_room_avdate);
 		
+		// 컨트롤러에서 -를 /으로 바꾸게되면 '2020/10/24'로 update되게 되겠죠??
+		// 근데 db에서는 String타입을 Date타입으로 변환해주는게
+		// TO_DATE()라는 것이고, 괄호 안에는 TO_DATE('문자열', '변환하고싶은날짜데이터형태')를 넣을 수 있어요
+		// 태현씨가 문자열에 넣을 값은 '2020/10/24'가 아니라 '20201024'입니다.(실제 mapper에서는 #{room_cpdate}로 넣어주셔야해요!)
+		// 그럼 아래의 명령어또한 의미가 없어요!
 		String new_room_avdate = prv_room_avdate.replaceAll("-", "/"); 
 		String new_room_cpdate = prv_room_cpdate.replaceAll("-", "/");
 		
@@ -61,8 +73,28 @@ public class RoomController {
 		logger.info(new_room_cpdate);
 		
 		
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		// 이제 '2020-10-24' 형태로 넘어온 값을 '20201024'형태로 변환하는 방법은 두가지가 있어요.
 		
+		// 1. format을 해준다.
+		// 아래와같이 작성을 해주게되면 yyyy-MM-dd 형태로 변환해주는 SimpleDatFormat객체를 format이라는 이름으로 선언만 해준거에요.
+		// 하지만 저희는 yyyyMMdd 형태를 원합니다.(위에 code67번째줄에서 설명했습니다.)
+		// 제가 변환할 형태를 yyyy-MM-dd밖에 안해봤는데 yyyyMMdd는 안해봐서 모르겠어요. 한번 해보시고 되시면 이렇게 써도 될 것 같아요.
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		// String 변수명 = format.(변환할값); 형태로 쓰면 돼요.
+		// 예) String room_cpdate = format.format(dto.getRoom_cpdate);
+		// 위처럼 진행을 하면 room_cpdate에 원하는 '20201024'형태로 값이 들어갔기 때문에
+		// room.setRoom_cpdate(room_cpdate); 를 작성해주시면 dto의 setter로 넘어가서 수정이 되게 됩니다.
+		// 그리고 얘를 int res = roomBiz.selectRoomInsert(room_dto);를 진행해주시면 RoomDaoImpl로 넘어가게 되겠죠??
+		
+		// 2. split을 해준다.
+		// input type="date"의 value는 "2020-10-24"로 넘어온다구 했죠??
+		// 그럼 저희는 "2020-10-24"라는 값이 필요하기 때문에 split("-")로 -를 기준으로 값을 배열에 담아주는거에요
+		// String[] cpdate = dto.getRoom_cpdate().split("-"); 를 하게되면 -를 기준으로 짤린 3개의 값이 배열에 담겨요.
+		// cpdate[0]는 "2020", cpdate[1]는 "10", cpdate[2]는 "24"가 들어가기 되겠죠?
+		// 최종적으로 필요한건 저 세개의 값을 모두 더한거에요
+		// String real_cpdatae = cpdate[0] + cpdate[1] + cpdate[2];를 해주면 real_cpdate라는 String변수에 2020,10,24가 다 더해진 값인
+		// "20201024"가 구해질거에요.
+		// 그리고 이를 똑같이 setter에 보내줘서 초기화해준 다음에 아래에서 selectRoomInsert를 해주게 되면 dao에서 mapper문을 실행하게되요.
 		
 //		room_dto.setRoom_cpdate(new_room_cpdate);
 //		room_dto.setRoom_avdate(new_room_avdate);
--- src/main/java/com/mvc/homeseek/controller/RoomDetailController.java
+++ src/main/java/com/mvc/homeseek/controller/RoomDetailController.java
@@ -74,45 +74,6 @@ public class RoomDetailController {
 		
 		model.addAttribute("room", room);
 		
-		// yyyy-MM-dd형식으로 format해주겠다는 객체
-		//SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		
-		// time이라는 변수에 ROOM테이블에 있는 room_regdate를 가져와서 format해준 후 담아준다.
-		//String regdatetime = format.format(room.getRoom_regdate());
-		//String cpdatetime = format.format(room.getRoom_cpdate());
-		//String avdatetime = format.format(room.getRoom_avdate());
-		// 2020-10-22
-		//System.out.println("controller에서 format" + regdatetime);
-		
-		// regdatetime에 담긴 값을 -를 기준으로 짤라서 regdate라는 배열에 하나씩 넣어준다.
-		// regdate[0] = 2020
-		// regdate[1] = 10
-		// regdate[2] = 22
-		//String[] regdate = regdatetime.split("-");
-		//String[] cpdate = cpdatetime.split("-");
-		//String[] avdate = avdatetime.split("-");
-		
-		
-		// room_regdate라는 Map객체를 생성해준다.
-		//Map<String, String> room_date = new HashMap<String, String>();
-		// room_regdate에 put해주는데 regdate_year이라는 이름으로 regdate에 0번지에 있는거 담아준다.
-		//room_date.put("regdate_year", regdate[0]);
-		//room_date.put("regdate_month", regdate[1]);
-		//room_date.put("regdate_day", regdate[2]);
-		// cpdate에 대한 year, month, day를 담아준다.
-		//room_date.put("cpdate_year", cpdate[0]);
-		//room_date.put("cpdate_month", cpdate[1]);
-		//room_date.put("cpdate_day", cpdate[2]);
-		// avdate에 대한 year, month, day를 담아준다.
-		//room_date.put("avdate_year", avdate[0]);
-		//room_date.put("avdate_month", avdate[1]);
-		//room_date.put("avdate_day", avdate[2]);
-		
-		// model로 보내준다.
-		//model.addAttribute("room_regdate", regdatetime);
-		//model.addAttribute("room_cpdate", cpdatetime);
-		//model.addAttribute("room_avdate", avdatetime);
-		
 		return "roomDetailUpdateForm";
 	}
 	
