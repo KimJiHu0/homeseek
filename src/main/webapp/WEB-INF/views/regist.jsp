38d7dbc1814cbf06523e235012a403b1253a4f5e
hig228 <hwangingyu228@gmail.com>
2020-10-24 16:05:09 +0900

    황인규 커밋: 회원가입 정규식표현 및 유효성 검사 추가(regist.js, regist.jsp)

--- src/main/webapp/WEB-INF/views/regist.jsp
+++ src/main/webapp/WEB-INF/views/regist.jsp
@@ -16,37 +16,41 @@
 	<form action="registres.do" method="post">
 			<div>
 				<label for="member_id">아이디</label>
-					<input type="text" id= "member_id" name="member_id" placeholder="ID"/>
-				<div class="check_font" id="name_check"></div>
+					<input type="text" id= "member_id" name="member_id" size="30" placeholder="소문자,숫자 조합(4~12)"/>
+				<div class="check_font" id="id_check"></div>
 			</div>
 			<div>
 				<label for="member_pw">비밀번호</label>
-					<input type="password" id="member_pw" name="member_pw" placeholder="PWD"/>
+					<input type="password" id="member_pw" name="member_pw" size="30" placeholder="소문자,숫자 조합(4~12)"/>
+					<div class="check_font" id="memberPw"></div>
 			</div>
 			<div>
 				<label for="member_pw">비밀번호 확인</label>
-					<input type="password" id="pw_check" name="pw_check" placeholder="PWD"/>
+					<input type="password" id="pw_check" name="pw_check" size="30" placeholder="소문자,숫자 조합(4~12)"/>
+					<div class="check_font" id="pwCheck"></div>
 			</div>
 			<div>
 				<label for="member_name">이름</label>
-					<input type="text" name="member_name" placeholder="NAME"/>
+					<input type="text" name="member_name" id="member_name" size="30" placeholder="NAME"/>
 			</div>
 			<div>
 				<label for="member_email">E-mail</label>
-					<input type="text" name="member_email" placeholder="E-MAIL"/>
+					<input type="text" name="member_email" id="member_email" size="30" placeholder="EX : hwangingyu@gmail.com"/>
+					<div class="check_font" id="emailCheck"></div>
 			</div>
 			<div>
 				<label for="member_phone">Phone</label>
-					<input type="text" name="member_phone" id="member_phone" placeholder="휴대폰 번호 입력"/>
+					<input type="text" name="member_phone" id="member_phone" size="30" placeholder="010-0000-0000"/>
 					<!--  <button onclick="sendSms();">휴대폰 인증</button>-->
 					<input type="button" name = "sendSms" id="sendSms" value="휴대폰 인증">
+					<div class="check_font" id="memberPhone"></div>
 			</div>
 			<div>
 					<input type="text" name="phone_check" id="phone_check" placeholder="인증 번호 입력"/>
 					<input type="button" name = "phoneCheck" id="phoneCheck" value="인증확인">
 			</div>
 			<div>
-					<input type="submit" value="회원등록"/>
+					<button class="btn btn-success" type="submit" id="submit">회원가입</button>
 					<input type="button" value="취소" onclick="location.href='main.do'"/>
 			</div>
 			
--- src/main/webapp/resources/js/regist.js
+++ src/main/webapp/resources/js/regist.js
@@ -1,7 +1,21 @@
+//모든 공백 체크 정규식
+	var empJ = /\s/g;
+	//아이디 정규식
+	var idJ = /^[a-z0-9]{4,12}$/;
+	// 비밀번호 정규식
+	var pwJ = /^[a-z0-9]{4,12}$/; 
+	// 이메일 검사 정규식
+	var mailJ = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+	// 휴대폰 번호 정규식
+	var phoneJ = /^01([0|1|6|7|8|9]?)?([0-9]{3,4})?([0-9]{4})$/;
+
 $(function(){
-//아이디 중복체크
+	document.getElementById("phone_check").style.display = "none";
+	document.getElementById("phoneCheck").style.display = "none";
+	//아이디 관련
     $('#member_id').blur(function(){
-        $.ajax({
+    	if (idJ.test($('#member_id').val())) {
+		 $.ajax({
 	     type:"POST",
 	     url:"checkid.do",
 	     data:{
@@ -10,77 +24,152 @@ $(function(){
 	     success:function(data){	//data : checkSignup에서 넘겨준 결과값
 	            if($.trim(data)=="YES"){
 	               if($('#member_id').val()!=''){ 
-	               	alert("사용가능한 아이디입니다.");
+	            	   $("#id_check").text("사용가능한 아이디입니다");
+	            	   $("#id_check").css("color", "blue");
 	               }
 	           	}else{
 	               if($('#member_id').val()!=''){
-	                  alert("중복된 아이디입니다.");
-	                  $('#member_id').val('');
-	                  $('#member_id').focus();
+	            	   $("#id_check").text("사용중인 아이디입니다");
+	            	   $("#id_check").css("color", "red");
+	            	   $("#reg_submit").attr("disabled", true);
+	                   $('#member_id').val('');
+	                   $('#member_id').focus();
 	               }
 	            }
 	         }
-	    }) 
+	    })
+    	} else {
+    		$('#id_check').text('숫자 or 문자로만  4~12자리 입력');
+    		$('#id_check').css('color', 'red');
+    		$('#member_id').val('');
+    		$('#member_id').focus();
+    	}
      })
+     //비밀번호 관련
+     $('#member_pw').blur(function(){
+    	 if (pwJ.test($('#member_pw').val())) {
+				$("#memberPw").text('사용 가능');
+				$('#memberPw').css('color', 'blue');
+		} else {
+			$('#memberPw').text('숫자 or 문자로만 4~12자리 입력');
+			$('#memberPw').css('color', 'red');
+		}
+     })
+     //비밀번호 확인 관련
      $('#pw_check').blur(function(){
+    	
 	   if($('#member_pw').val() != $('#pw_check').val()){
 	    	if($('#pw_check').val()!=''){
-		    alert("비밀번호가 일치하지 않습니다.");
+	    		$("#pwCheck").text("비밀번호가 일치하지 않습니다");
+	    		$("#pwCheck").css("color", "red");
 	    	    $('#pw_check').val('');
 	          $('#member_pw').focus();
 	       }
+	    }else{
+	    	$("#pwCheck").text("비밀번호가 일치합니다");
+    		$("#pwCheck").css("color", "blue");
 	    }
-	})  
+	})
+		$("#member_email").blur(function() {
+		if (mailJ.test($(this).val())) {
+				$("#emailCheck").text('사용 가능');
+		} else {
+			$('#emailCheck').text('이메일 양식에 맞춰주세요');
+			$('#emailCheck').css('color', 'red');
+		}
+	});
+  
+	
+	//본인인증 번호 보내기
 	$('#sendSms').click(function(){
-	    var phoneNumber = $('#member_phone').val();
-		alert("인증번호 발송")
+		if (phoneJ.test($(this).val())) {
+			if($('#member_phone').val() != ""){
+				document.getElementById("phone_check").style.display = "block";
+				document.getElementById("phoneCheck").style.display = "block";
+				var phoneNumber = $('#member_phone').val();
+				alert("인증번호 발송")
 
 
-	    $.ajax({
-	        type: "POST",
-	        url: "sendsms.do",
-	        data: {
-	            "phoneNumber" : phoneNumber
-	        },
-	        success: function(res){
-	            $('#phoneCheck').click(function(){
-	                if($.trim(res) == $('#phone_check').val()){
-	                    /*Swal.fire(
-	                        '인증성공!',
-	                        '휴대폰 인증이 정상적으로 완료되었습니다.',
-	                        'success'
-	                    )*/
-	                	alert("휴대폰 인증이 정상적으로 완료되었습니다.");
-
-	                   /* $.ajax({
-	                        type: "GET",
-	                        url: "/update/phone",
-	                        data: {
-	                            "phoneNumber" : $('#inputPhoneNumber').val()
-	                        }
-	                    })
-	                    document.location.href="/home";*/
-	                }else{
-	                    /*Swal.fire({
-	                        icon: 'error',
-	                        title: '인증오류',
-	                        text: '인증번호가 올바르지 않습니다!',
-	                        footer: '<a href="/home">다음에 인증하기</a>'
-	                    })*/
-	                	alert("인증 실패!");
-	                	$('#phone_check').val('');
-		                  $('#phone_check').focus();
-	                }
-	            })
-
+				$.ajax({
+					type: "POST",
+					url: "sendsms.do",
+					data: {
+						"phoneNumber" : phoneNumber
+					},
+					success: function(res){
+						//인증번호 확인
+						$('#phoneCheck').click(function(){
+							if($.trim(res) == $('#phone_check').val()){
+							
+								alert("휴대폰 인증이 정상적으로 완료되었습니다.");
+							}else if($.trim(res) != $('#phone_check').val() || $('#phone_check').val('')){
+								alert("인증 실패!");
+								$('#phone_check').val('');
+								$('#phone_check').focus();
+							}
+					
+						})
+				
 	        
-	        }
-	    })
+					}
+			
+				})
+			}
+	}else{
+			$('#memberPhone').text('올바른 번호를 입력해주세요');
+			$('#memberPhone').css('color', 'red');
+			$('#memberPhone').val('');
+			$('#memberPhone').focus();
+		}
 	});
 
+	
+    //회원 등록 버튼
+    $("#submit").on ("click", function(){
+		if($("#member_id").val()==""){
+			alert("아이디를 입력해주세요.");
+			$("#userId").focus();
+			return false;
+		}
+		else if($("#member_pw").val()==""){
+			alert("비밀번호를 입력해주세요.");
+			$("#member_pw").focus();
+			return false;
+		}
+		else if($("#pw_check").val()==""){
+			alert("비밀번호를 한번 더 입력해주세요.");
+			$("#pw_check").focus();
+			return false;
+		}
+		else if($("#member_name").val()==""){
+			alert("성명을 입력해주세요.");
+			$("#member_name").focus();
+			return false;
+		}
+		else if($("#member_email").val()==""){
+			alert("이메일을 입력해주세요.");
+			$("#member_email").focus();
+			return false;
+		}
+		else if($("#member_phone").val()==""){
+			alert("휴대폰 번호를 입력해주세요");
+			$("#member_phone").focus();
+			return false;
+		}
+		else if($("#phone_check").val()==""){
+			alert("휴대폰 인증번호를 입력해주세요.");
+			$("#phone_check").focus();
+			return false;
+		}
+	});
+
+	
+		
+	
+})
 
 
-});
+
 
 
 
