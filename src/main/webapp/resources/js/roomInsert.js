9c8cfc5e0b87e3278007e2d98dfbe83df78a09d5
ffolabear <kth6699@naver.com>
2020-10-26 14:48:54 +0900

    김태현 : 인터셉터 수정, 카카오맵, 도로명주소 연동중

---
+++ src/main/webapp/resources/js/roomInsert.js
@@ -0,0 +1,15 @@
+function goPopup(){
+	//경로는 시스템에 맞게 수정하여 사용
+	//호출된 페이지(jusopopup.jsp)에서 실제 주소검색URL(http://www.juso.go.kr/addrlink/addrLinkUrl.do)를 //호출하게 됩니다.
+
+	var pop = window.open("insertpopup.do","pop","width=570,height=420, scrollbars=yes, resizable=yes");
+	
+}
+
+
+function jusoCallBack(roadFullAddr,roadAddrPart1,addrDetail,roadAddrPart2,engAddr, jibunAddr, zipNo, admCd, rnMgtSn, bdMgtSn , detBdNmList, bdNm, bdKdcd, siNm, sggNm, emdNm, liNm, rn, udrtYn, buldMnnm, buldSlno, mtYn, lnbrMnnm, lnbrSlno, emdNo){
+	// 2017년 2월 제공항목이 추가되었습니다. 원하시는 항목을 추가하여 사용하시면 됩니다.
+	// 팝업페이지에서 주소입력한 정보를 받아서, 현 페이지에 정보를 등록합니다.
+	document.form.roadFullAddr.value = roadFullAddr; 
+	
+}
--- src/main/webapp/resources/js/summerNote-roomInsert.js
+++ src/main/webapp/resources/js/summerNote-roomInsert.js
@@ -8,5 +8,30 @@ $(document).ready(function() {
            maxHeight: 300,
            focus: true, 
            lang : 'ko-KR',
+           callbacks: {
+				onImageUpload: function(files, editor, welEditable) {
+		            for (var i = files.length - 1; i >= 0; i--) {
+		            	sendFile(files[i], this);
+		            }
+		        }
+			}
    });
+       
 });
+
+function sendFile(file, el) {
+	var form_data = new FormData();
+  	form_data.append('file', file);
+  	$.ajax({
+    	data: form_data,
+    	type: "POST",
+    	url: './profileImage.mpf',
+    	cache: false,
+    	contentType: false,
+    	enctype: 'multipart/form-data',
+    	processData: false,
+    	success: function(img_name) {
+      		$(el).summernote('editor.insertImage', img_name);
+    	}
+  	});
+}
