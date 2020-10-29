2dfdd808cb25cc88f532b999049146f9a3b165bb
ffolabear <kth6699@naver.com>
2020-10-29 13:18:24 +0900

    김태현 : 써머노트 수정

--- src/main/webapp/resources/js/summerNote-roomInsert.js
+++ src/main/webapp/resources/js/summerNote-roomInsert.js
@@ -10,15 +10,19 @@ function sendFile(file, el) {
 		enctype : 'multipart/form-data',
 		processData : false,
 		success : function(img_name) {
-			$(el).summernote('editor.insertImage', data.url);
+			$(el).summernote('editor.insertImage',img_name);
 			console.log("이미지 처리중");
 		}
 	});
 }
+
 $(function() {
 	$('#summernote').summernote({
 		 	placeholder: '최대 500자 작성 가능합니다.',
 	        height: 300,
+	        minHeight: 300,             
+	    	maxHeight: 300,             
+	        focus: true,
 	        lang: 'ko-KR',
 	        callbacks: {
 	        	onImageUpload: function(files, editor, welEditable) {
@@ -30,3 +34,6 @@ $(function() {
 	        }
 	 });
 });
+
+//--------------------------------------------------------------------------------------------------------
+
