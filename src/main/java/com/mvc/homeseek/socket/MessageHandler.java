eadc008a82d14dbbad0e73bf73777d1f8b2406f0
KimJiHu0 <65142418+KimJiHu0@users.noreply.github.com>
2020-11-10 15:53:18 +0900

    김지후 : interceptor을 다시 선언하고 allowed-origins="*" 추가해새ㅓ https에 배포

--- src/main/java/com/mvc/homeseek/socket/MessageHandler.java
+++ src/main/java/com/mvc/homeseek/socket/MessageHandler.java
@@ -38,13 +38,13 @@ public class MessageHandler extends TextWebSocketHandler {
 
 		// userId는 맨 아래 선언되어있는 메소드인데 현재 session에 담긴(로그인한)
 		// 아이디를 가져오기 위해서 선언해주었다.
-		//String userId = UserId(session);
+		String userId = UserId(session);
 
 		// logger.info("\n 6. userId의 값은 뭐니? : " + userId);
 
-		logger.info(" \n [ " + session.getId() + " ] 연결됨 ");
+		logger.info(" \n [ " + userId + " ] 연결됨 ");
 		// 방금 로그인에 성공한 애를 담아준다.
-		userSessionMap.put(session.getId(), session); // 8 8
+		userSessionMap.put(userId, session); // 8 8
 
 		for (int i = 0; i < userSessionMap.size(); i++) {
 			logger.info("\n 7. userSessionMap에 담겨있는 key값은? : " + userSessionMap.keySet());
@@ -100,7 +100,7 @@ public class MessageHandler extends TextWebSocketHandler {
 				  String message_reid = strs[2];
 			  
 			  // 작성자가 로그인해있다면 // 쪽지 받는 사람
-			  WebSocketSession messageReid = userSessionMap.get(session.getId());
+			  WebSocketSession messageReid = userSessionMap.get(message_reid);
 			  
 			  logger.info("\n boardWriterSession? : " + messageReid.toString());
 			  logger.info("\n message? : " + cmd);
@@ -108,7 +108,7 @@ public class MessageHandler extends TextWebSocketHandler {
 			  if ("message".equals(cmd) && messageReid != null) {
 			  
 			  TextMessage tmpMsg = new TextMessage( 
-					  message_senid + "님이 " + message_reid + "님에게 쪽지를 보냈습니다."); 
+					  message_senid + "(보내는사람)님이 " + message_reid + "(받는사람)님에게 쪽지를 보냈습니다."); 
 			  messageReid.sendMessage(tmpMsg); // 이 부분이 쪽지를 보낸사람의 id에게 send해주는 부분?
 			  }
 			}			 
@@ -135,24 +135,24 @@ public class MessageHandler extends TextWebSocketHandler {
 
 	// 웹소켓 id가져오기
 
-	//@SuppressWarnings("unused")
-	//private String UserId(WebSocketSession session) {
-	//	logger.info("\n 2. UserId메소드를 통해서 들어온 session의 id는? : " + session.getId());
+	@SuppressWarnings("unused")
+	private String UserId(WebSocketSession session) {
+		logger.info("\n 2. UserId메소드를 통해서 들어온 session의 id는? : " + session.getId());
 
-	//	Map<String, Object> httpSession = session.getAttributes();
-	//	logger.info("\n 3. session에 담겨있는 getAttribute를 통해 가져온 Map의 key값은 ? : " + httpSession.keySet());
-	//	logger.info("\n 3. session에 담겨있는 getAttribute를 통해 가져온 Map의 value값은 ? : " + httpSession.values());
-	//	MemberDto loginUser = (MemberDto) httpSession.get("login");
+		Map<String, Object> httpSession = session.getAttributes();
+		logger.info("\n 3. session에 담겨있는 getAttribute를 통해 가져온 Map의 key값은 ? : " + httpSession.keySet());
+		logger.info("\n 3. session에 담겨있는 getAttribute를 통해 가져온 Map의 value값은 ? : " + httpSession.values());
+		MemberDto loginUser = (MemberDto) httpSession.get("login");
 
-	//	logger.info("\n 4. loginUser이라는 MemberDto에 담겨있는 값은? : " + loginUser.toString());
-
-	//	if (loginUser != null) {
-	//		logger.info("\n 5. 현재 loginUser이 null이 아니니???");
-	//		return loginUser.getMember_id();
-	//	} else {
-	//		logger.info("\n 5. 현재 loginUser이 null이니???");
-	//		return session.getId();
-	//	}
-	//}
+		logger.info("\n 4. loginUser이라는 MemberDto에 담겨있는 값은? : " + loginUser.toString());
+		
+		if (loginUser != null) {
+			logger.info("\n 5. 현재 loginUser이 null이 아니니???");
+			return loginUser.getMember_id();
+		} else {
+			logger.info("\n 5. 현재 loginUser이 null이니???");
+			return session.getId();
+		}
+	}
 
 }
